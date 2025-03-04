47a7602748d7f99a2c9fd8e00f4370e7c82efaf4
kainat <Kainatn002@gmail.com>
2025-03-04 21:28:04 +0100

    completed the code. working on end screen

--- index.js
+++ index.js
@@ -6,12 +6,20 @@ document.getElementById("start-button").addEventListener("click", function () {
 });
 
 const cardImages = [
-  { name: "aquaman", image: "images/aquaman.jpg" },
-  { name: "batman", image: "images/batman.jpg" },
-  { name: "captain-america", image: "images/captain-america.jpg" },
-  { name: "fantastic-four", image: "images/fantastic-four.jpg" },
-  { name: "thor", image: "images/thor.jpg" },
-  { name: "ironman", image: "images/ironman.jpg" },
+  { name: "HERO-1", image: "images/hero-1.jpg" },
+  { name: "HERO-2", image: "images/hero-2.jpg" },
+  { name: "HERO-3", image: "images/hero-3.jpg" },
+  { name: "HERO-4", image: "images/hero-4.jpg" },
+  { name: "HERO-5", image: "images/hero-5.jpg" },
+  { name: "HERO-6", image: "images/hero-6.jpg" },
+  { name: "HERO-7", image: "images/hero-7.jpg" },
+  { name: "HERO-8", image: "images/hero-8.jpg" },
+  { name: "HERO-9", image: "images/hero-9.jpg" },
+  { name: "HERO-10", image: "images/hero-10.jpg" },
+  { name: "HERO-11", image: "images/hero-11.jpg" },
+  { name: "HERO-12", image: "images/hero-12.jpg" },
+  { name: "HERO-13", image: "images/hero-13.jpg" },
+  { name: "HERO-14", image: "images/hero-14.jpg" },
 ];
 const gameCards = [...cardImages, ...cardImages].map((card, index) => {
   const cardElement = document.createElement("div");
--- memoryboard.js
+++ memoryboard.js
@@ -1,18 +1,12 @@
 class MemoryGame {
   constructor() {
     this.flippedCards = [];
-    this.moves = 0;
-    this.score = 0;
-    this.maxMoves = 20;
-    // this.revealCard();
+    this.moves = document.querySelector("#moves-count");
+    this.score = document.querySelector("#score-count");
+    this.maxMoves = 5;
+    
   }
-  // revealCard(){
-  //   gameCards.forEach((card) =>
-  //       card.addEventListener("click", (event) => {
-  //         card.classList.add("flipped");
-  //       })
-  //     );
-  // }
+
   flipCard(card) {
     if (this.flippedCards.length < 2) {
       card.classList.add("flipped");
@@ -22,21 +16,51 @@ class MemoryGame {
     if (this.flippedCards.length === 2) {
       this.checkForMatch();
     }
+    this.maximumMoves();
   }
   checkForMatch() {
     const [card1, card2] = this.flippedCards;
 
     if (card1.dataset.name === card2.dataset.name) {
+      card1.classList.add("matched");
+      card2.classList.add("matched");
       this.flippedCards = [];
-      this.score++;
+      this.score.textContent = parseInt(this.score.textContent) + 1;
+      this.checkForWin();
     } else {
       setTimeout(() => {
-        console.log("insideTimeout");
         card1.classList.toggle("flipped");
         card2.classList.toggle("flipped");
         this.flippedCards = [];
       }, 1000);
     }
-    this.moves++;
+    this.moves.textContent = parseInt(this.moves.textContent) + 1;
+  }
+
+  maximumMoves() {
+    if (parseInt(this.moves.textContent) >= this.maxMoves) {
+      this.endGame(true);
+    }
+  }
+  endGame(isWon) {
+    document.getElementById("memory-board").style.display = "none";
+    document.getElementById("end-screen").style.display = "flex";
+    if (isWon) {
+      document.getElementById("win-comment").style.display = "block";
+    } else {
+      document.getElementById("lost-comment").style.display = "block";
+    }
+  }
+  checkForWin() {
+    const totalPairs = cardImages.length;
+    const matchedPairs = document.querySelectorAll(".card.matched").length / 2;
+    if (matchedPairs === totalPairs) {
+      this.endGame(true);
+    }
   }
 }
+document
+  .getElementById("restart-button")
+  .addEventListener("click", function () {
+    location.reload();
+  });
